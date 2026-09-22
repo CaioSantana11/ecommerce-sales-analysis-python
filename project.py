031f7ef45d12a4b3d0be0ff2d4ebbde1d0235c50
Caio <caiooliver2007@hotmail.com>
2026-09-22 11:25:37 -0300

    feat: import pandas, numpy, matplotlib, seaborn, random and datetime libs

---
+++ project.py
@@ -0,0 +1,18 @@
+#Manipulação de dados em tabelas
+import pandas as pd  # pyright: ignore[reportMissingModuleSource]
+
+#Numpy para operações matemáticas e arrays
+import numpy as np
+
+#Matplotlib para geração de gráficos
+import matplotlib.pyplot as plt 
+
+#Seaborn para visualização estatística de dados
+import seaborn as sns
+
+#Biblioteca Random para geração de números aleatórios
+import random
+
+#Manipulação de datas e intervalos de tempo
+from datetime import datetime, timedelta
+
